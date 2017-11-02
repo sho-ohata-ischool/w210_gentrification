74c432976aed3f542763c66c151bfb813b3dfd6c
Geoff <geoffstirling@users.noreply.github.com>
2017-11-01 20:30:22 -0700

    File updates

--- Visualization/server.R
+++ Visualization/server.R
@@ -2,6 +2,13 @@ library(shiny)
 library(leaflet)
 library(shinydashboard)
 
+gdata <- read.csv("Income_Home_Prices_ZIP.csv")
+
+gdata$long <- as.numeric(gdata$Longitude)
+gdata$lat <- as.numeric(gdata$Latitude)
+
+data.SP <- SpatialPointsDataFrame(gdata[,c(23, 24)], gdata[,-c(23,24)])
+
 
 shinyServer(function(input, output) {
   output$mymap <- renderLeaflet({
@@ -12,9 +19,9 @@ shinyServer(function(input, output) {
       setView(lng=-73.935, lat=40.690, zoom=10) %>% 
       #addMarkers(data = gdata, lng = ~Longitude, lat = ~Latitude, popup = ~Neighborhood)
       #addMarkers(data = gdata, lng = ~Longitude, lat = ~Latitude, popup = ~paste(ZIP, Neighborhood, AGI, sep = ", "))
-        addMarkers(data = gdata, clusterOptions = markerClusterOptions(), lng = ~Longitude, lat = ~Latitude,
-                  popup = ~paste("<b>ZIP Code:</b>", ZIP, "<br>", "<b>Neighborhood:</b>", Neighborhood,"<br>",
-                                "<b>Year</b>", Year,"<br>","<b>Average Income</b>", AGI, sep = " "))
+      addMarkers(data = gdata, clusterOptions = markerClusterOptions(), lng = ~Longitude, lat = ~Latitude,
+                popup = ~paste("<b>ZIP Code:</b>", ZIP, "<br>", "<b>Neighborhood:</b>", Neighborhood,"<br>",
+                              "<b>Year</b>", Year,"<br>","<b>Average Income</b>", AGI, sep = " "))
   })
   
 })
