1840a592150210eddff5d836d431426acde4c94e
Geoff <geoffstirling@users.noreply.github.com>
2017-11-03 12:23:13 -0700

    Push layout fix (map/table now side by side)
    Format fixes, improvements to table

--- Visualization/server.R
+++ Visualization/server.R
@@ -1,11 +1,14 @@
 library(shiny)
 library(leaflet)
 library(shinydashboard)
+library(DT)
 
 gdata <- read.csv("Income_Home_Prices_ZIP.csv")
 
 gdata$long <- as.numeric(gdata$Longitude)
 gdata$lat <- as.numeric(gdata$Latitude)
+
+gdatatable <- datatable(gdata[as.numeric(c(1, 2, 4, 8, 27)), drop = FALSE])
 
 data.SP <- SpatialPointsDataFrame(gdata[,c(23, 24)], gdata[,-c(23,24)])
 
@@ -13,15 +16,20 @@ data.SP <- SpatialPointsDataFrame(gdata[,c(23, 24)], gdata[,-c(23,24)])
 shinyServer(function(input, output) {
   output$mymap <- renderLeaflet({
     # define the leaflet map object
+      sliderInput("Year", "Gentrification Prediction by Year:", min = as.Date("2000","%Y"), max = as.Date("2030","%Y"), 
+                value = as.Date("2020", "%Y"), timeFormat = "%Y")                     
       leaflet() %>% 
       #addTiles() %>%
-        addProviderTiles("OpenMapSurfer.Roads") %>%
+        addProviderTiles("Esri.WorldTopoMap") %>%
       setView(lng=-73.935, lat=40.690, zoom=10) %>% 
       #addMarkers(data = gdata, lng = ~Longitude, lat = ~Latitude, popup = ~Neighborhood)
       #addMarkers(data = gdata, lng = ~Longitude, lat = ~Latitude, popup = ~paste(ZIP, Neighborhood, AGI, sep = ", "))
       addMarkers(data = gdata, clusterOptions = markerClusterOptions(), lng = ~Longitude, lat = ~Latitude,
                 popup = ~paste("<b>ZIP Code:</b>", ZIP, "<br>", "<b>Neighborhood:</b>", Neighborhood,"<br>",
                               "<b>Year</b>", Year,"<br>","<b>Average Income</b>", AGI, sep = " "))
+   
   })
-  
-})
+
+  output$mytable <- renderDataTable(gdatatable, scrollX = TRUE, target = column, cols = c(1,3))
+  })
+
--- Visualization/ui.R
+++ Visualization/ui.R
@@ -1,20 +1,62 @@
 library(shiny)
 library(leaflet)
+library(shinydashboard)
+library(DT)
 
 
-shinyUI(fluidPage(
-  shinyUI(navbarPage("gentrifAI",
-                     tabPanel("Map View"),
-                     tabPanel("ZIP Code Data"),
-                     tabPanel("About the gentrifAI team"),
-  position = "static-top",
-  sliderInput("Year", "Gentrification Prediction by Year:", min = as.Date("2000","%Y"), max = as.Date("2030","%Y"), 
-              value = as.Date("2020", "%Y"), timeFormat = "%Y"),                     
-  #Likely won't use the pull down menu option here, but leaving the code in and commented out
-  #for now, in case we change our minds
-  #selectInput("Forecast", "Forecast Year",
-  #            list("Years" = c("2020", "2025", "2030"))),
-  leafletOutput("mymap"))
-          
+header <- dashboardHeader(
+  title = "gentrifAI")
+
+sidebar <- dashboardSidebar()
+
+
+body <- dashboardBody(
+  fixedRow(
+    # column(width = 3,
+    #        box(width = NULL, solidHeader = TRUE, status = "primary",
+    #            sliderInput("Year", "Gentrification Prediction by Year:", min = as.Date("2000","%Y"), max = as.Date("2030","%Y"), 
+    #                        value = as.Date("2020", "%Y"), timeFormat = "%Y"),
+  
+    br(), br(),
+    column(width = 6,
+           box(width = NULL, solidHeader = TRUE, status = "primary",
+               sliderInput("Year", "Gentrification Prediction by Year:", min = as.Date("2000","%Y"), max = as.Date("2030","%Y"), 
+                           value = as.Date("2020", "%Y"), timeFormat = "%Y"), br(),
+               leafletOutput("mymap")
+               )),
+    
+  column(width = 5, offset = "1",
+           box(width = NULL, solidHeader = TRUE,
+               dataTableOutput("mytable"))
+  )
   ))
+
+
+ui <- dashboardPage(
+  header,
+  sidebar,
+  body
 )
+
+
+# shinyUI(fluidPage(
+#   shinyUI(navbarPage("gentrifAI",
+#                      tabPanel("Map View",
+#                        sliderInput("Year", "Gentrification Prediction by Year:", min = as.Date("2000","%Y"), max = as.Date("2030","%Y"), 
+#                                   value = as.Date("2020", "%Y"), timeFormat = "%Y"),
+#                        mainPanel(
+#                          leafletOutput("mymap",width = "100%", height = "100%")
+#                        )), 
+#                      tabPanel("ZIP Code Data"),
+#                      tabPanel("About the gentrifAI team"),
+#   position = "static-top",
+#   #sliderInput("Year", "Gentrification Prediction by Year:", min = as.Date("2000","%Y"), max = as.Date("2030","%Y"), 
+#   #            value = as.Date("2020", "%Y"), timeFormat = "%Y"),                     
+#   #Likely won't use the pull down menu option here, but leaving the code in and commented out
+#   #for now, in case we change our minds
+#   #selectInput("Forecast", "Forecast Year",
+#   #            list("Years" = c("2020", "2025", "2030"))),
+#   leafletOutput("mymap"))
+#           
+#   ))
+# )
