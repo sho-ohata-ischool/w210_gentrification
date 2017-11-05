e2f5e4f814c41317bb3e5dbab4b010e2a595b7d9
Geoff <geoffstirling@users.noreply.github.com>
2017-11-05 14:20:43 -0800

    Latest files for data viz

--- Visualization/ui.R
+++ Visualization/ui.R
@@ -4,14 +4,22 @@ library(shinydashboard)
 library(DT)
 
 
-header <- dashboardHeader(
-  title = "gentrifAI")
+#header <- dashboardHeader(tags$li(class = "dropdown", tags$a(target="_blank", tags$img(height = "20px", alt="SNAP Logo", 
+#                                                                                                         src="white_logo.png", height = '300', width = '50'))))
 
-sidebar <- dashboardSidebar()
+header <- dashboardHeader(title = "")
+
+sidebar <- dashboardSidebar(
+  sidebarMenu(width = 1, tags$img(src = "white_logo.png", height = 100, width = 225),
+    menuItem("Map View", tabname = "mapview", selected = TRUE, icon = icon("map")),
+    menuItem("Charts", tabname = "charts", selected = TRUE, icon = icon("line-chart")),
+    menuItem("About gentrifAI", tabname = "aboutus", selected = TRUE, icon = icon("user-circle")),
+  textOutput("res")
+))
 
 
 body <- dashboardBody(
-  fixedRow(
+  fluidRow(
     # column(width = 3,
     #        box(width = NULL, solidHeader = TRUE, status = "primary",
     #            sliderInput("Year", "Gentrification Prediction by Year:", min = as.Date("2000","%Y"), max = as.Date("2030","%Y"), 
@@ -19,15 +27,16 @@ body <- dashboardBody(
   
     br(), br(),
     column(width = 6,
-           box(width = NULL, solidHeader = TRUE, status = "primary",
-               sliderInput("Year", "Gentrification Prediction by Year:", min = as.Date("2000","%Y"), max = as.Date("2030","%Y"), 
-                           value = as.Date("2020", "%Y"), timeFormat = "%Y"), br(),
+           box(title = "Gentrification Map of New York City", width = NULL, solidHeader = TRUE, status = "primary",
+               sliderInput("pickyear", "Select a Year:", min = as.Date("2000","%Y"), max = as.Date("2030","%Y"), 
+                           value = as.Date("2010", "%Y"), timeFormat = "%Y"), tags$br(),
                leafletOutput("mymap")
                )),
-    
-  column(width = 5, offset = "1",
-           box(width = NULL, solidHeader = TRUE,
-               dataTableOutput("mytable"))
+
+
+    column(width = 6,
+         box(title = "Top Gentrified ZIP Codes", width = NULL, solidHeader = TRUE, status = "primary",
+             dataTableOutput("mytable"))
   )
   ))
 
@@ -38,25 +47,3 @@ ui <- dashboardPage(
   body
 )
 
-
-# shinyUI(fluidPage(
-#   shinyUI(navbarPage("gentrifAI",
-#                      tabPanel("Map View",
-#                        sliderInput("Year", "Gentrification Prediction by Year:", min = as.Date("2000","%Y"), max = as.Date("2030","%Y"), 
-#                                   value = as.Date("2020", "%Y"), timeFormat = "%Y"),
-#                        mainPanel(
-#                          leafletOutput("mymap",width = "100%", height = "100%")
-#                        )), 
-#                      tabPanel("ZIP Code Data"),
-#                      tabPanel("About the gentrifAI team"),
-#   position = "static-top",
-#   #sliderInput("Year", "Gentrification Prediction by Year:", min = as.Date("2000","%Y"), max = as.Date("2030","%Y"), 
-#   #            value = as.Date("2020", "%Y"), timeFormat = "%Y"),                     
-#   #Likely won't use the pull down menu option here, but leaving the code in and commented out
-#   #for now, in case we change our minds
-#   #selectInput("Forecast", "Forecast Year",
-#   #            list("Years" = c("2020", "2025", "2030"))),
-#   leafletOutput("mymap"))
-#           
-#   ))
-# )
