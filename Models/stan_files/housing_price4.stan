9fa96bf33a7e1faf85cda09f77bfb4788ea53fca
w210 <w210@virtualserver01.Sho-Ohata-s-Account.cloud>
2017-12-04 05:31:36 +0000

    updates of stan files

--- Models/stan_files/housing_price4.stan
+++ Models/stan_files/housing_price4.stan
@@ -3,24 +3,27 @@ data {
   int N_pred; //number of observations for new prediction
   int J; //the number of neighborhoods
   int K; //number of columns in the model matrix
-  //int id[N]; //vector of neighborhoods indices
-  //int id_pred[N_pred]; //vector of neighborhoods prediction indices
+  int id[N]; //vector of neighborhoods indices
+  int id_pred[N_pred]; //vector of neighborhoods prediction indices
   int boro[N]; //vector of borough
   int boro_pred[N_pred];
   int B; //number of boroughs
-  int S; //number of categories for amount of subway stations
-  int station[N]; //vector of station categorical variable
+  //int S; //number of categories for amount of subway stations
+  //int station[N]; //vector of station categorical variable
   //int station_pred[N_pred]; 
-  int water[N]; //vector of waterfront binary variable
+  //int water[N]; //vector of waterfront binary variable
   matrix[N,K] X; //the model matrix
   vector[N] y; //the response variable e.g. zillow price
+  vector[N] predictor_crime; 
+  vector[N] prev_zillow;
   matrix[N_pred,K] X_pred; //data being fed in for prediction
+  vector[N_pred] X_pred_crime;
+  vector[N_pred] X_prev_zillow;
 }
 parameters {
-  //vector[K] betas[B]; //the regression parameters
   vector[K] betas[B]; //the regression parameters
-  //#**real gamma[J];
-  //real tau; 
+  real gamma[B];
+  real tau[J]; 
   real <lower=0, upper=2> phi; //the variance parameter
 }
 transformed parameters {
@@ -30,16 +33,18 @@ transformed parameters {
   for (n in 1:N){
     //mu[n] = exp(X[n] * betas[boro[n]] + gamma[boro[n], station[n]]); //using the log link 
     //#**mu[n] = exp(X[n] * betas[boro[n]] + gamma[boro[n]]); //using the log link 
-    mu[n] = exp(X[n] * betas[boro[n]]); //using the log link
+    //mu[n] = exp(X[n] * betas[boro[n]] + gamma[boro[n]] + predictor_crime[n] * tau[id[n]]); //using the log link
+    mu[n] = prev_zillow[n] + exp(X[n] * betas[boro[n]] + gamma[boro[n]] + predictor_crime[n] * tau[id[n]]); //using the log link
   }
   alpha = mu .* mu / phi; 
   beta = mu / phi;
 }
 model {
   
-  //for (j in 1:J){
-    //gamma[j] ~ cauchy(0.5,1); //prior for the intercept following Gelman 2008
-  //}
+  for (j in 1:J){
+    //gamma[j] ~ cauchy(0,1); //prior for the intercept following Gelman 2008
+    tau[j] ~ cauchy(0,2.5);
+  }
   
   //tau ~ cauchy(0,2.5);
   
@@ -51,7 +56,7 @@ model {
   //}
   
   for (b in 1:B) {
-    //#**gamma[b] ~ cauchy(0,2.5);
+    gamma[b] ~ cauchy(0,1);
     for(i in 1:K) {
       betas[b, i] ~ cauchy(0,2.5);//prior for the slopes following Gelman 2008
     }
@@ -67,7 +72,7 @@ generated quantities {
  for(n in 1:N_pred){
   //mu_pred[n] = exp(X_pred[n] * betas[boro_pred[n]] + gamma[boro_pred[n], station_pred[n]]);
   //#**mu_pred[n] = exp(X_pred[n] * betas[boro_pred[n]] + gamma[boro_pred[n]]);
-  mu_pred[n] = exp(X_pred[n] * betas[boro_pred[n]]);
+  mu_pred[n] = X_prev_zillow[n] + exp(X_pred[n] * betas[boro_pred[n]] + gamma[boro_pred[n]] + X_pred_crime[n] * tau[id_pred[n]]);
   alpha_pred[n] = mu_pred[n] * mu_pred[n] / phi;
   beta_pred[n] = mu_pred[n] / phi;
   y_sim[n] = gamma_rng(alpha_pred[n],beta_pred[n]); //posterior draws to get posterior predictive checks
--- Models/stan_model_income4.R
+++ Models/stan_model_income4.R
@@ -4,7 +4,7 @@ library(data.table)
 
 options(mc.cores = parallel::detectCores())
 
-stan_file <- file.path(getwd(),"Models","stan_files","housing_price3.stan") #where the STAN model is saved
+stan_file <- file.path(getwd(),"Models","stan_files","housing_price4.stan") #where the STAN model is saved
 df <- data.table(read.csv((file.path(getwd(),"Data","Income_Home_Prices_ZIP_v3.csv"))))
 df[,CRIME_COUNTperSqMile:=CRIME_COUNT/LandSqMile]
 df[,log_AGI:=log(AGIadj2015)]
@@ -17,9 +17,9 @@ df[,Num_stat_cat:=ifelse(Number.of.Subway.Lines.Serving.ZIP==0, 0, ifelse(Number
 df_zillow <- fread(file.path(getwd(), "Data", "Zillow2016_2017_Yearly_byZipcode.csv"))
 df_zillow <- df_zillow[`ZIP Code` %in% unique(df[,ZIP])]
 colnames(df_zillow)[1:2] <- c("Year", "ZIP")
-df_zillow <- df_zillow[,c("Year", "ZIP", "ZillowAdj2")]
-df_zillow <- rbind(df[,.(Year, ZIP, ZillowAdj2)], df_zillow)
+df_zillow <- rbind(df[,.(Year, ZIP, Zillow_value, ZillowAdj, Zillow_value2, ZillowAdj2)], df_zillow)
 df_zillow[,ZillowAdj2_lag:=shift(ZillowAdj2,1),by=ZIP]
+df_zillow[,Zillow_change:=ZillowAdj2 - ZillowAdj2_lag]
 
 ## Crime data for prediction of corresponding year
 df_crime <- fread(file.path(getwd(), "Data", "NYC violent crime yearly count.csv"))
@@ -40,7 +40,7 @@ df_permit[,DM_lag2:=log(shift(as.double(DM/LandSqMile),2)+1.001), by=ZIP]
 df_permit <- df_permit[Year==2016 | Year==2017]
 
 ## Create input data to stan
-df_mod <- df[,.(ZIP, Borough, Year, log_AGI, AdjacentIncome, Bordering.Water, ZillowAdj2, 
+df_mod <- df[,.(ZIP, Borough, Neighborhood, Year, log_AGI, AdjacentIncome, Bordering.Water, ZillowAdj2, 
                 AdjacentZillow2, log_crime, Num_stat_cat, A1, A2, DM, NB, LandSqMile, Proximity)]
 df_mod[,log_prev_crime:=shift(log_crime,1),by=ZIP]
 df_mod[,log_prev_AGI:=shift(log_AGI,1),by=ZIP]
@@ -49,20 +49,39 @@ df_mod[,NB_lag1:=log(shift(as.double(NB),1)+1.001),by=ZIP]
 df_mod[,A1_lag1:=log(shift(as.double(A1),1)+1.001),by=ZIP]
 df_mod[,A2_lag1:=log(shift(as.double(A2),1)+1.001),by=ZIP]
 df_mod[,DM_lag2:=log(shift(as.double(DM/LandSqMile),2)+1.001), by=ZIP]
+#df_mod[,DM_lag2:=log(shift(as.double(DM),2)+1.001), by=ZIP]
 
 df_mod <- df_mod[complete.cases(df_mod)] ##for now not back-filling data
 df_mod[,intercept:=rep(1,nrow(df_mod))]
-predictors <- as.matrix(df_mod[,.(intercept,prev_zillow, log_prev_crime, NB_lag1,A1_lag1,A2_lag1, DM_lag2, Proximity)])
+#predictors <- as.matrix(df_mod[,.(intercept,prev_zillow, log_prev_crime, NB_lag1,A1_lag1,A2_lag1, DM_lag2, Proximity)])
+#predictors <- as.matrix(df_mod[,.(prev_zillow, log_prev_crime, NB_lag1,A1_lag1,A2_lag1, DM_lag2, Proximity)])
+predictors <- as.matrix(df_mod[,.(NB_lag1,A1_lag1,A2_lag1, DM_lag2, Proximity)])
+predictor_prev_zillow <- df_mod$prev_zillow
+predictor_crime <- df_mod$log_prev_crime
 response <- df_mod$ZillowAdj2
 
 ## Create data for prediction
+##df_pred <- df[,.(ZIP, Year, ZillowAdj2, log_crime, A1_lag1=log(as.double(A1)+1.001), NB_lag1=log(as.double(NB)+1.001), DM_lag2=shift(log(as.double(DM+1.001)), 1))]
+#df_pred <- df[,.(ZIP, Year, ZillowAdj2, log_crime)]
+##df_pred <- df_pred[Year==2015]
+##merged <- cbind(merge(df_zillow[,.(ZIP, Year, ZillowAdj2)][Year==2016], df_crime, by.x=c("ZIP", "Year"), by.y=c("ZIP", "Year")), df_permit[Year==2017][,.(A1_lag1, NB_lag1,DM_lag2)])
+#merged <- merge(df_zillow[,.(ZIP, Year, ZillowAdj2)][Year==2016], df_crime, by.x=c("ZIP", "Year"), by.y=c("ZIP", "Year"))
+##merged <- merged[,.(ZIP, Year, ZillowAdj2, log_crime, A1_lag1, NB_lag1,DM_lag2)]
+##df_pred <- merge(rbind(df_pred, merged), unique(df_mod[,.(ZIP, Borough, Num_stat_cat)]), by=c("ZIP"))
+##df_pred$A2_log <- df_permit[,(A2_log)]
+
 df_pred <- merge(merge(df_zillow[,.(ZIP, Year, ZillowAdj2_lag)][Year==2016 | Year==2017], df_crime, by=c("ZIP", "Year")), 
                  df_permit[,.(ZIP, Year, NB_lag1, A1_lag1, A2_lag1, DM_lag2)],by=c("ZIP", "Year"))
 df_pred[,intercept:=rep(1,nrow(df_pred))]
-df_pred <- merge(df_pred, unique(df[,.(ZIP, Borough, Neighborhood)]), by=c("ZIP"))
+df_pred <- merge(df_pred, unique(df[,.(ZIP, Neighborhood, Borough, Num_stat_cat)]), by=c("ZIP"))
 prox <- df[df$Year==2015][,.(ZIP, Proximity)]
 df_pred <- merge(df_pred, prox, by=c("ZIP"))
-X_pred <- as.matrix(df_pred[,.(intercept, ZillowAdj2_lag, log_crime, NB_lag1, A1_lag1, A2_lag1, DM_lag2, Proximity)])
+#X_pred <- as.matrix(df_pred[,.(ZillowAdj2_lag, log_crime, NB_lag1, A1_lag1, A2_lag1, DM_lag2, Proximity)])
+#X_pred <- as.matrix(df_pred[,.(ZillowAdj2_lag, NB_lag1, A1_lag1, A2_lag1, DM_lag2, Proximity)])
+X_pred <- as.matrix(df_pred[,.(NB_lag1, A1_lag1, A2_lag1, DM_lag2, Proximity)])
+X_pred_crime <- df_pred$log_crime
+X_pred_prev_zillow <- df_pred$ZillowAdj2_lag
+#X_pred <- as.matrix(df_pred[,.(intercept, ZillowAdj2_lag, log_crime, NB_lag1, A1_lag1, A2_lag1, DM_lag2, Proximity)])
 
 ##Parameters to be passed to stan
 N <- nrow(df_mod) #number of observations
@@ -81,8 +100,13 @@ id_pred <- as.numeric(as.factor(df_pred$Neighborhood)) ## each group, i.e. neigh
 zip_levels <- levels(as.factor(df_mod$ZIP)) ##to map back id to zip
 
 #run the model
-stan_data <- list(N=N,N_pred=N_pred,J=J,K=K,zip=id,zip_pred=id_pred,boro=boro,
-                  boro_pred=boro_pred,B=B,water=water,station=station, S=S,X=predictors,X_pred=X_pred,y=response)
+stan_data <- list(N=N,N_pred=N_pred,J=J,K=K,id=id,id_pred=id_pred,
+                  boro=boro,boro_pred=boro_pred,B=B,
+                  ##water=water,
+                  station=station, S=S, station_pred=station_pred,
+                  X_pred_crime=X_pred_crime, predictor_crime=predictor_crime,
+                  prev_zillow=predictor_prev_zillow, X_prev_zillow=X_pred_prev_zillow,
+                  X=predictors,X_pred=X_pred,y=response)
 if (exists("m_hier")){ rm(m_hier) }
 m_hier<-stan(file=stan_file, data = stan_data, chains=4)
 
